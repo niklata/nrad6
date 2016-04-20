5f16318ed6633fb0ddcc14e21bd57f09ce2ba158
Nicholas J. Kain <nicholas@kain.us>
2016-04-20 01:03:38 -0400

    dhcp6: Refactor in preparation for handling multiple message types.

--- dhcp6.cpp
+++ dhcp6.cpp
@@ -1,110 +1,11 @@
-#include <iterator>
 #include <nk/format.hpp>
 #include "multicast6.hpp"
-#include "netbits.hpp"
 #include "dhcp6.hpp"
 #include "attach_bpf.h"
 
 namespace ba = boost::asio;
 
 static auto mc6_alldhcp_ras = ba::ip::address_v6::from_string("ff02::1:2");
-
-enum class dhcp6_msgtype {
-    unknown = 0,
-    solicit = 1,
-    advertise = 2,
-    request = 3,
-    confirm = 4,
-    renew = 5,
-    rebind = 6,
-    reply = 7,
-    release = 8,
-    decline = 9,
-    reconfigure = 10,
-    information_request = 11,
-    relay_forward = 12,
-    relay_reply = 13,
-};
-
-class dhcp6_header
-{
-public:
-    dhcp6_header() { std::fill(data_, data_ + sizeof data_, 0); }
-    uint32_t xid() const { return data_[1] << 16 | data_[2] << 8 | data_[3]; }
-    void xid(uint32_t v) {
-        data_[1] = v >> 16 & 0xff;
-        data_[2] = v >> 8 & 0xff;
-        data_[3] = v & 0xff;
-    }
-    dhcp6_msgtype msg_type() const {
-        const auto dt = data_[0];
-        if (dt >= 1 && dt <= 13)
-            return static_cast<dhcp6_msgtype>(dt);
-        return dhcp6_msgtype::unknown;
-    };
-    void msg_type(dhcp6_msgtype v) { data_[0] = static_cast<uint8_t>(v); }
-    static const std::size_t size = 4;
-    friend std::istream& operator>>(std::istream &is, dhcp6_header &header)
-    {
-        is.read(reinterpret_cast<char *>(header.data_), size);
-        return is;
-    }
-    friend std::ostream& operator<<(std::ostream &os,
-                                    const dhcp6_header &header)
-    {
-        return os.write(reinterpret_cast<const char *>(header.data_), size);
-    }
-private:
-    uint8_t data_[4];
-};
-
-class dhcp6_opt
-{
-public:
-    dhcp6_opt() { std::fill(data_, data_ + sizeof data_, 0); }
-    uint16_t type() const { return decode16be(data_); }
-    uint16_t length() const { return decode16be(data_ + 2); }
-    void type(uint16_t v) { encode16be(v, data_); }
-    void length(uint16_t v) { encode16be(v, data_ + 2); }
-    static const std::size_t size = 4;
-    friend std::istream& operator>>(std::istream &is, dhcp6_opt &header)
-    {
-        is.read(reinterpret_cast<char *>(header.data_), size);
-        return is;
-    }
-    friend std::ostream& operator<<(std::ostream &os,
-                                    const dhcp6_opt &header)
-    {
-        return os.write(reinterpret_cast<const char *>(header.data_), size);
-    }
-private:
-    uint8_t data_[4];
-};
-
-class dhcp6_hwaddr_duid
-{
-public:
-    dhcp6_hwaddr_duid() {
-        std::fill(data_, data_ + sizeof data_, 0);
-        // Ethernet is assumed.
-        data_[1] = 3;
-        data_[3] = 1;
-    }
-    void macaddr(const char v[6]) { memcpy(data_ + 4, v, 6); }
-    static const std::size_t size = 10;
-    friend std::istream& operator>>(std::istream &is, dhcp6_hwaddr_duid &header)
-    {
-        is.read(reinterpret_cast<char *>(header.data_), size);
-        return is;
-    }
-    friend std::ostream& operator<<(std::ostream &os,
-                                    const dhcp6_hwaddr_duid &header)
-    {
-        return os.write(reinterpret_cast<const char *>(header.data_), size);
-    }
-private:
-    uint8_t data_[10];
-};
 
 D6Listener::D6Listener(ba::io_service &io_service,
                        const std::string &ifname,
@@ -235,6 +136,96 @@ void create_ntp6_fqdns_blob()
     }
 }
 
+void D6Listener::handle_information_request(const d6msg_state &d6s,
+                                            ba::streambuf &send_buffer)
+{
+    dhcp6_header send_d6hdr;
+    send_d6hdr.msg_type(dhcp6_msgtype::reply);
+    send_d6hdr.xid(d6s.header.xid());
+
+    std::ostream os(&send_buffer);
+    os << send_d6hdr;
+
+    dhcp6_opt send_serverid;
+    send_serverid.type(2);
+    send_serverid.length(10);
+    os << send_serverid;
+    dhcp6_hwaddr_duid send_hwduid;
+    send_hwduid.macaddr(macaddr_);
+    os << send_hwduid;
+
+    if (d6s.client_duid.size()) {
+        dhcp6_opt send_clientid;
+        send_clientid.type(1);
+        send_clientid.length(d6s.client_duid.size());
+        os << send_clientid;
+        for (const auto &i: d6s.client_duid)
+            os << i;
+    }
+
+    // If opt_req.size() == 0 then send DnsServers, DomainList,
+    // and NtpServer.  Otherwise, for each of these types,
+    // see if it is in the opt_req before adding it to the reply.
+
+    if ((!d6s.optreq_exists || d6s.optreq_dns) && dns6_servers.size()) {
+        dhcp6_opt send_dns;
+        send_dns.type(23);
+        send_dns.length(dns6_servers.size() * 16);
+        os << send_dns;
+        for (const auto &i: dns6_servers) {
+            auto d6b = i.to_bytes();
+            for (const auto &j: d6b)
+                os << j;
+        }
+    }
+
+    if ((!d6s.optreq_exists || d6s.optreq_dns_search)
+        && dns_search_blob.size()) {
+        dhcp6_opt send_dns_search;
+        send_dns_search.type(24);
+        send_dns_search.length(dns_search_blob.size());
+        os << send_dns_search;
+        for (const auto &i: dns_search_blob)
+            os << i;
+    }
+    auto n6s_size = ntp6_servers.size();
+    auto n6m_size = ntp6_multicasts.size();
+    auto n6d_size = ntp6_fqdns_blob.size();
+    if ((!d6s.optreq_exists || d6s.optreq_ntp)
+        && (n6s_size || n6m_size || n6d_size)) {
+        uint16_t len(0);
+        dhcp6_opt send_ntp;
+        send_ntp.type(56);
+        if (n6s_size)
+            len += 4 + n6s_size * 16;
+        if (n6m_size)
+            len += 4 + n6m_size * 16;
+        if (n6d_size)
+            len += n6d_size;
+        send_ntp.length(len);
+        os << send_ntp;
+
+        for (const auto &i: ntp6_servers) {
+            uint16_t soc(1);
+            uint16_t sol(16);
+            os << soc << sol;
+            auto n6b = i.to_bytes();
+            for (const auto &j: n6b)
+                os << j;
+        }
+        for (const auto &i: ntp6_multicasts) {
+            uint16_t soc(2);
+            uint16_t sol(16);
+            os << soc << sol;
+            auto n6b = i.to_bytes();
+            for (const auto &j: n6b)
+                os << j;
+        }
+        for (const auto &i: ntp6_fqdns_blob)
+            os << i;
+    }
+}
+
 void D6Listener::start_receive()
 {
     recv_buffer_.consume(recv_buffer_.size());
@@ -257,28 +248,19 @@ void D6Listener::start_receive()
              }
 
              std::istream is(&recv_buffer_);
-             dhcp6_header dhcp6_hdr;
-             is >> dhcp6_hdr;
+             d6msg_state d6s;
+             is >> d6s.header;
              bytes_left -= dhcp6_header::size;
 
-             fmt::print(stderr, "dhcp message type: {}\n", static_cast<uint8_t>(dhcp6_hdr.msg_type()));
+             fmt::print(stderr, "dhcp message type: {}\n", static_cast<uint8_t>(d6s.header.msg_type()));
 
              if (!using_bpf_) {
-                 if (dhcp6_hdr.msg_type() != dhcp6_msgtype::information_request) {
+                 if (d6s.header.msg_type() != dhcp6_msgtype::information_request) {
                      fmt::print(stderr, "DHCP6 Message type not InfoReq\n");
                      start_receive();
                      return;
                  }
              }
-
-             std::vector<uint8_t> client_duid;
-             uint16_t elapsed_time;
-
-             bool optreq_exists(false);
-             bool optreq_dns(false);
-             bool optreq_dns_search(false);
-             bool optreq_info_refresh_time(false);
-             bool optreq_ntp(false);
 
              while (bytes_left >= 4) {
                  //fmt::print(stderr, "bytes_left={}\n", bytes_left);
@@ -297,9 +279,9 @@ void D6Listener::start_receive()
                  }
 
                  if (ot == 1) { // ClientID
-                     client_duid.reserve(l);
+                     d6s.client_duid.reserve(l);
                      while (l--) {
-                         client_duid.push_back(is.get());
+                         d6s.client_duid.push_back(is.get());
                          --bytes_left;
                      }
                  } else if (ot == 6) { // OptionRequest
@@ -311,7 +293,7 @@ void D6Listener::start_receive()
                          }
                          continue;
                      }
-                     optreq_exists = true;
+                     d6s.optreq_exists = true;
                      l /= 2;
                      while (l--) {
                          char b[2];
@@ -321,10 +303,10 @@ void D6Listener::start_receive()
                          uint16_t v;
                          memcpy(&v, b, 2);
                          switch (v) {
-                         case 23: optreq_dns = true; break;
-                         case 24: optreq_dns_search = true; break;
-                         case 32: optreq_info_refresh_time = true; break;
-                         case 56: optreq_ntp = true; break;
+                         case 23: d6s.optreq_dns = true; break;
+                         case 24: d6s.optreq_dns_search = true; break;
+                         case 32: d6s.optreq_info_refresh_time = true; break;
+                         case 56: d6s.optreq_ntp = true; break;
                          default: break;
                          }
                      }
@@ -342,7 +324,7 @@ void D6Listener::start_receive()
                      b[1] = is.get();
                      b[0] = is.get();
                      bytes_left -= 2;
-                     memcpy(&elapsed_time, b, 2);
+                     memcpy(&d6s.elapsed_time, b, 2);
                  } else {
                      while (l--) {
                          is.get();
@@ -351,96 +333,15 @@ void D6Listener::start_receive()
                  }
              }
 
-             dhcp6_header send_d6hdr;
-             send_d6hdr.msg_type(dhcp6_msgtype::reply);
-             send_d6hdr.xid(dhcp6_hdr.xid());
-
              ba::streambuf send_buffer;
-             std::ostream os(&send_buffer);
-             os << send_d6hdr;
-
-             dhcp6_opt send_serverid;
-             send_serverid.type(2);
-             send_serverid.length(10);
-             os << send_serverid;
-             dhcp6_hwaddr_duid send_hwduid;
-             send_hwduid.macaddr(macaddr_);
-             os << send_hwduid;
-
-             if (client_duid.size()) {
-                 dhcp6_opt send_clientid;
-                 send_clientid.type(1);
-                 send_clientid.length(client_duid.size());
-                 os << send_clientid;
-                 for (const auto &i: client_duid)
-                     os << i;
-             }
-
-             // If opt_req.size() == 0 then send DnsServers, DomainList,
-             // and NtpServer.  Otherwise, for each of these types,
-             // see if it is in the opt_req before adding it to the reply.
-
-             if ((!optreq_exists || optreq_dns) && dns6_servers.size()) {
-                 dhcp6_opt send_dns;
-                 send_dns.type(23);
-                 send_dns.length(dns6_servers.size() * 16);
-                 os << send_dns;
-                 for (const auto &i: dns6_servers) {
-                     auto d6b = i.to_bytes();
-                     for (const auto &j: d6b)
-                         os << j;
-                 }
-             }
-
-             if ((!optreq_exists || optreq_dns_search)
-                 && dns_search_blob.size()) {
-                 dhcp6_opt send_dns_search;
-                 send_dns_search.type(24);
-                 send_dns_search.length(dns_search_blob.size());
-                 os << send_dns_search;
-                 for (const auto &i: dns_search_blob)
-                     os << i;
-             }
-             auto n6s_size = ntp6_servers.size();
-             auto n6m_size = ntp6_multicasts.size();
-             auto n6d_size = ntp6_fqdns_blob.size();
-             if ((!optreq_exists || optreq_ntp)
-                 && (n6s_size || n6m_size || n6d_size)) {
-                 uint16_t len(0);
-                 dhcp6_opt send_ntp;
-                 send_ntp.type(56);
-                 if (n6s_size)
-                     len += 4 + n6s_size * 16;
-                 if (n6m_size)
-                     len += 4 + n6m_size * 16;
-                 if (n6d_size)
-                     len += n6d_size;
-                 send_ntp.length(len);
-                 os << send_ntp;
-
-                 for (const auto &i: ntp6_servers) {
-                     uint16_t soc(1);
-                     uint16_t sol(16);
-                     os << soc << sol;
-                     auto n6b = i.to_bytes();
-                     for (const auto &j: n6b)
-                         os << j;
-                 }
-                 for (const auto &i: ntp6_multicasts) {
-                     uint16_t soc(2);
-                     uint16_t sol(16);
-                     os << soc << sol;
-                     auto n6b = i.to_bytes();
-                     for (const auto &j: n6b)
-                         os << j;
-                 }
-                 for (const auto &i: ntp6_fqdns_blob)
-                     os << i;
+             switch (d6s.header.msg_type()) {
+             case dhcp6_msgtype::information_request:
+                 handle_information_request(d6s, send_buffer); break;
+             default: start_receive(); return;
              }
 
              boost::system::error_code ec;
              socket_.send_to(send_buffer.data(), remote_endpoint_, 0, ec);
-
              start_receive();
          });
 }
--- dhcp6.hpp
+++ dhcp6.hpp
@@ -3,7 +3,9 @@
 
 #include <string>
 #include <stdint.h>
+#include <iterator>
 #include <boost/asio.hpp>
+#include "netbits.hpp"
 
 class D6Listener
 {
@@ -12,6 +14,118 @@ public:
                const std::string &ifname,
                const char macaddr[6]);
 private:
+    enum class dhcp6_msgtype {
+        unknown = 0,
+        solicit = 1,
+        advertise = 2,
+        request = 3,
+        confirm = 4,
+        renew = 5,
+        rebind = 6,
+        reply = 7,
+        release = 8,
+        decline = 9,
+        reconfigure = 10,
+        information_request = 11,
+        relay_forward = 12,
+        relay_reply = 13,
+    };
+
+    class dhcp6_header
+    {
+    public:
+        dhcp6_header() { std::fill(data_, data_ + sizeof data_, 0); }
+        uint32_t xid() const { return data_[1] << 16 | data_[2] << 8 | data_[3]; }
+        void xid(uint32_t v) {
+            data_[1] = v >> 16 & 0xff;
+            data_[2] = v >> 8 & 0xff;
+            data_[3] = v & 0xff;
+        }
+        dhcp6_msgtype msg_type() const {
+            const auto dt = data_[0];
+            if (dt >= 1 && dt <= 13)
+                return static_cast<dhcp6_msgtype>(dt);
+            return dhcp6_msgtype::unknown;
+        };
+        void msg_type(dhcp6_msgtype v) { data_[0] = static_cast<uint8_t>(v); }
+        static const std::size_t size = 4;
+        friend std::istream& operator>>(std::istream &is, dhcp6_header &header)
+        {
+            is.read(reinterpret_cast<char *>(header.data_), size);
+            return is;
+        }
+        friend std::ostream& operator<<(std::ostream &os,
+                                        const dhcp6_header &header)
+        {
+            return os.write(reinterpret_cast<const char *>(header.data_), size);
+        }
+    private:
+        uint8_t data_[4];
+    };
+
+    class dhcp6_opt
+    {
+    public:
+        dhcp6_opt() { std::fill(data_, data_ + sizeof data_, 0); }
+        uint16_t type() const { return decode16be(data_); }
+        uint16_t length() const { return decode16be(data_ + 2); }
+        void type(uint16_t v) { encode16be(v, data_); }
+        void length(uint16_t v) { encode16be(v, data_ + 2); }
+        static const std::size_t size = 4;
+        friend std::istream& operator>>(std::istream &is, dhcp6_opt &header)
+        {
+            is.read(reinterpret_cast<char *>(header.data_), size);
+            return is;
+        }
+        friend std::ostream& operator<<(std::ostream &os,
+                                        const dhcp6_opt &header)
+        {
+            return os.write(reinterpret_cast<const char *>(header.data_), size);
+        }
+    private:
+        uint8_t data_[4];
+    };
+
+    class dhcp6_hwaddr_duid
+    {
+    public:
+        dhcp6_hwaddr_duid() {
+            std::fill(data_, data_ + sizeof data_, 0);
+            // Ethernet is assumed.
+            data_[1] = 3;
+            data_[3] = 1;
+        }
+        void macaddr(const char v[6]) { memcpy(data_ + 4, v, 6); }
+        static const std::size_t size = 10;
+        friend std::istream& operator>>(std::istream &is, dhcp6_hwaddr_duid &header)
+        {
+            is.read(reinterpret_cast<char *>(header.data_), size);
+            return is;
+        }
+        friend std::ostream& operator<<(std::ostream &os,
+                                        const dhcp6_hwaddr_duid &header)
+        {
+            return os.write(reinterpret_cast<const char *>(header.data_), size);
+        }
+    private:
+        uint8_t data_[10];
+    };
+    struct d6msg_state
+    {
+        d6msg_state() : optreq_exists(false), optreq_dns(false), optreq_dns_search(false),
+                        optreq_info_refresh_time(false), optreq_ntp(false) {}
+        dhcp6_header header;
+        std::vector<uint8_t> client_duid;
+        uint16_t elapsed_time;
+
+        bool optreq_exists:1;
+        bool optreq_dns:1;
+        bool optreq_dns_search:1;
+        bool optreq_info_refresh_time:1;
+        bool optreq_ntp:1;
+    };
+
+    void handle_information_request(const d6msg_state &d6s, boost::asio::streambuf &send_buffer);
     void start_receive();
     void attach_bpf(int fd);
     boost::asio::ip::udp::socket socket_;
